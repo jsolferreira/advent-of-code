d82f68edf37317c01c0ed154709a80a0aedf36c8
jmferreira <jorge.ferreira@first-global.com>
2022-01-04 14:42:56 +0000

    2021/day-03

--- 2021/day-03/src/main.rs
+++ 2021/day-03/src/main.rs
@@ -38,28 +38,64 @@ fn part_one(input: &Vec<Vec<u32>>) -> usize {
     total
 }
 
-fn part_two(input: &Vec<Vec<u32>>) -> usize {
-    let mut total = 0;
+fn part_two(input: &Vec<Vec<u32>>) -> u32 {
+    let oxygen_generator_rating = get_oxygen_generator_rating(&input, 0);
+    let co2_scrubber_rating = get_co2_scrubber_rating(&input, 0);
 
-    let mut x = input;
+    oxygen_generator_rating * co2_scrubber_rating
+}
 
-    for i in (0..5).rev() {
-        let mut total_0 = 0;
-        let mut total_1 = 0;
+fn get_oxygen_generator_rating(input: &Vec<Vec<u32>>, bit_pos: usize) -> u32 {
+    let mut arr_of_zeroes = Vec::new();
+    let mut arr_of_ones = Vec::new();
 
-        for bits in input {
-            if bits[i] == 0 {
-                total_0 += 1;
-            } else if bits[i] == 1 {
-                total_1 += 1;
-            }
-        }
+    if input.len() == 1 {
+        return binary_to_decimal(&input[0]);
+    }
 
-        if total_1 > total_0 {
-            println!("{:?}", input.into_iter().filter(|c| c[0] == 1).collect::<Vec<&Vec<u32>>>());
-            //x = input.into_iter().filter(|c| c[0] == 0).collect::<Vec<Vec<u32>>>();
+    for bits in input {
+        if bits[bit_pos] == 0 {
+            arr_of_zeroes.push(bits.clone());
+        } else if bits[bit_pos] == 1 {
+            arr_of_ones.push(bits.clone());
         }
     }
 
-    total
+    if arr_of_zeroes.len() > arr_of_ones.len() {
+        return get_oxygen_generator_rating(&arr_of_zeroes, bit_pos + 1);
+    } else {
+        return get_oxygen_generator_rating(&arr_of_ones, bit_pos + 1);
+    }
+}
+
+fn get_co2_scrubber_rating(input: &Vec<Vec<u32>>, bit_pos: usize) -> u32 {
+    let mut arr_of_zeroes = Vec::new();
+    let mut arr_of_ones = Vec::new();
+
+    if input.len() == 1 {
+        return binary_to_decimal(&input[0]);
+    }
+
+    for bits in input {
+        if bits[bit_pos] == 0 {
+            arr_of_zeroes.push(bits.clone());
+        } else if bits[bit_pos] == 1 {
+            arr_of_ones.push(bits.clone());
+        }
+    }
+
+    if arr_of_zeroes.len() <= arr_of_ones.len() {
+        return get_co2_scrubber_rating(&arr_of_zeroes, bit_pos + 1);
+    } else {
+        return get_co2_scrubber_rating(&arr_of_ones, bit_pos + 1);
+    }
+}
+
+fn binary_to_decimal(binary: &[u32]) -> u32 {
+    let mut decimal = 0;
+    for bit in 0..binary.len() {
+        decimal += 2_u32.pow((binary.len() - 1 - bit) as u32) * binary[bit];
+    }
+
+    decimal
 }
